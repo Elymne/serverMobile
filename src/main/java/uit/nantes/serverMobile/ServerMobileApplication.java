69f8fa01495bee3d7b7a02d24887e1c57c0d3a91
Elymne <Elymne@MSI>
2019-02-11 12:34:59 +0100

    first commit

---
+++ src/main/java/uit/nantes/serverMobile/ServerMobileApplication.java
@@ -0,0 +1,14 @@
+package uit.nantes.serverMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerMobileApplication.class, args);
+	}
+
+}
+
