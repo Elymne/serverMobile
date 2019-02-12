5913106250df9ca42bf70be386097870d280d9b5
Djurdjevic Sacha <>
2019-02-12 10:18:27 +0100

    add requestmapping controller getUserById

--- src/main/java/uit/nantes/serverMobile/application/config/Config.java
+++ src/main/java/uit/nantes/serverMobile/application/config/Config.java
@@ -1,5 +1,6 @@
 package uit.nantes.serverMobile.application.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import uit.nantes.serverMobile.domain.UserService;
  */
 @Configuration
 @EnableJpaRepositories(basePackages="uit.nantes.serverMobile.infra.jpa")
+@EntityScan(basePackages = "uit.nantes.serverMobile.api.entities")
 public class Config {
 
     @Bean
--- src/main/java/uit/nantes/serverMobile/application/controller/UserController.java
+++ src/main/java/uit/nantes/serverMobile/application/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import uit.nantes.serverMobile.api.entities.User;
 import uit.nantes.serverMobile.domain.UserService;
 
