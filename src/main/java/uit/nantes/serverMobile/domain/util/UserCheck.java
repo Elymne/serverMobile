a7d58dc943bd20a632d0a57950876e358b496d65
Djurdjevic Sacha <Elymne@MSI>
2019-02-13 15:59:22 +0100

    correct bugs

--- src/main/java/uit/nantes/serverMobile/domain/util/UserCheck.java
+++ src/main/java/uit/nantes/serverMobile/domain/util/UserCheck.java
@@ -9,12 +9,13 @@ public class UserCheck {
 
     public static boolean checkUpdate(User user, User userUpdate) {
         boolean result = true;
-        if (user.getEmail().equals(userUpdate.getEmail())) {
-            if (user.getPassword().equals(userUpdate.getPassword())) {
-                if (user.getPseudo().equals(userUpdate.getPseudo())) {
-                    result = false;
-                }
-            }
+        if(user.getPseudo().equals(userUpdate.getPseudo())
+                || user.getEmail().equals(userUpdate.getEmail())
+                || user.getPseudo().isBlank()
+                || user.getEmail().isBlank()
+                || user.getPassword().isBlank()
+                || user.getPassword().length() < 5){
+            result = false;
         }
         return result;
     }
